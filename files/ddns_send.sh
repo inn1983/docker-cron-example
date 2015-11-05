3806e0f697b590220c1fdc38097f243b21d63d41
inn1983 <inngi2011@gmail.com>
2015-11-06 03:31:02 +0900

    fix some bugs.

--- files/ddns_send.sh
+++ files/ddns_send.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-wget http://www.mydns.jp/login.html --http-user=$ENV_DOCKER_DDNS_USER --http-passwd=$ENV_DOCKER_DDNS_PW -O /dev/null
+wget http://www.mydns.jp/login.html --http-user=mydns510358 --http-passwd=mgiB7VSMZqp -O /dev/null
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --net host -v /home/log:/log/ -e ENV_DOCKER_DDNS_USER=mydns510358 -e ENV_DOCKER_DDNS_PW=mgiB7VSMZqp --restart=always cubieboard.info:5000/armv7hf_cron:ddns_send
+docker run -d --net host -v /home/zuiki/log:/log/ --restart=always cubieboard.info:5000/armv7hf_cron:ddns_send

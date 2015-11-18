510aafc8f3ae0feec1e96484e318dd0f91a8b254
inn1983 <inngi2011@gmail.com>
2015-11-18 14:09:03 +0900

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d --net host -v /home/zuiki/log:/log/ -v /etc/network:/etc/network --restart=always cubieboard.info:5000/armv7hf_cron:wificlient
+docker run -d --privileged --net host -v /home/zuiki/log:/log/ -v /etc/network:/etc/network --restart=always cubieboard.info:5000/armv7hf_cron:wificlient
 
